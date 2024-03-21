dd4f88ab9fe29cacd6ee539445084b0f2c911e31
rsmadhavan <rsmadhavan2000@gmail.com>
2024-03-21 20:40:06 +0530

    Create form with html and add styles
    formhandler logs the input to console

--- frontend-react-vite/code-snippets/src/components/CodeSubmissionForm.jsx
+++ frontend-react-vite/code-snippets/src/components/CodeSubmissionForm.jsx
@@ -1,8 +1,67 @@
+import { useState } from "react";
+import '../styles/CodeSubmissionForm.css'
+
 function CodeSubmissionForm () {
+  const [username, setUsername] = useState('');
+  const [language, setLanguage] = useState('');
+  const [input, setInput] = useState('');
+  const [sourceCode,setSourceCode] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', {username,language,input,sourceCode});
+  }
   return (
-    <>
-    <h1>This is a form Blehgg</h1>
-    </>
+   <div className="form-container">
+    <h2>Code Submission Form</h2>
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="username">Username: </label>
+        <input 
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="language">Preferred Coding Language: </label>
+        <select 
+          id="language"
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+          required
+        >
+          <option value="">Select Language</option>
+          <option value="c++">C++</option>
+          <option value="java">Java</option>
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+          <label htmlFor="input">Standard Input:</label>
+          <input
+            type="text"
+            id="input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="source-code">Source Code:</label>
+          <textarea
+            id="source-code"
+            value={sourceCode}
+            onChange={(e) => setSourceCode(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>      
+    </form>
+   </div> 
   );
 }
 
--- frontend-react-vite/code-snippets/src/pages/FormPage.jsx
+++ frontend-react-vite/code-snippets/src/pages/FormPage.jsx
@@ -1,7 +1,9 @@
+import CodeSubmissionForm from "../components/CodeSubmissionForm";
 function FormPage() {
   return (
     <div>
       <h1>Form Page</h1>
+      <CodeSubmissionForm/>
       {/* Add content for the form page */}
     </div>
   );
